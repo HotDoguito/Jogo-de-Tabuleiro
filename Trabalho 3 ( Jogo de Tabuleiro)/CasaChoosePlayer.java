6917c7e55c834913096bbb13dd1d944ccde52825
Miguel <miguelsonmaia@gmail.com>
2026-05-17 12:23:13 -0300

    Refactor houseEffect method to improve player selection

--- Trabalho 3 ( Jogo de Tabuleiro)/CasaChoosePlayer.java
+++ Trabalho 3 ( Jogo de Tabuleiro)/CasaChoosePlayer.java
@@ -7,15 +7,14 @@ public class CasaChoosePlayer extends Casa {
         System.out.println("Jogador " + player.nome + ", você entrou em uma casa Especial!\nEscolha um jogador para voltar ao início do tabuleiro.");
     }
     public void houseEffect(Jogador player,  ArrayList<Jogador> jogadores){
-        System.out.println("Digite o nome ou a cor do jogador que voltará para o início do tabuleiro: ");
-        String resp = input.nextLine();
+       String resp;
        while(true){
         System.out.println("Digite o nome ou a cor do jogador que voltará para o início do tabuleiro: ");
         resp = input.nextLine();
         for(int i = 0; i < jogadores.size(); i++){
             if((jogadores.get(i).nome) == resp || jogadores.get(i).cor == resp){
                 System.out.println("Jogador escolhido com sucesso!");
-            jogadores.get(i).posicao = 0;
+            jogadores.set(i).posicao = 0;
             return;
         }
         System.out.println("Resposta inválida!");
